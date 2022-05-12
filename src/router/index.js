b72ee59b5efd1a56ec2dd3fd7bb933b48830f9a2
张猛 <zhangmeng@c5game.com>
2022-05-12 17:17:05 +0800

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {path: '/', redirect: '/home'},
+  {path: '/login', component: Login },
+  {path: '/home', component: Home},
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/member',
+    redirect: '/member/list',
+    name: 'member',
+    meta: {title: '用户管理', icon: 'ums-role'}
   }
 ]
 
